2cd197d9f79a7be689ae125e74cd7201605777be
Serge <sshibkojavadev@gmail.com>
2025-02-26 22:26:07 +0800

    init commit

---
+++ src/main/java/ru/sshibko/chat_gpt_telegram_bot/ChatGptTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package ru.sshibko.chat_gpt_telegram_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatGptTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatGptTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sshibko/chat_gpt_telegram_bot/ChatGptTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sshibko.chat_gpt_telegram_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatGptTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
